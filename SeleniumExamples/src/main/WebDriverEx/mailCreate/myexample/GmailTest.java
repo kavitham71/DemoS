a33b8f25ac8e1ed0e8fb28457a3d779b490670a0
kavitham71 <42564649+kavitham71@users.noreply.github.com>
2019-02-26 15:04:40 +0530

    Update GmailTest.java

--- SeleniumExamples/src/main/WebDriverEx/mailCreate/myexample/GmailTest.java
+++ SeleniumExamples/src/main/WebDriverEx/mailCreate/myexample/GmailTest.java
@@ -13,7 +13,7 @@ public class GmailTest {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.gecko.driver",
-				"/home/vedams/Kavitha/eclipse_projects2/selenium_ex/GuruProjectEx/lib/Drivers/geckodriver/geckodriver");
+				"/home/hostname/eclipse_projects2/selenium_ex/GuruProjectEx/lib/Drivers/geckodriver/geckodriver");
 		WebDriver driver = new FirefoxDriver();
 
 		String baseUrl = "http://gmail.com";
@@ -27,12 +27,12 @@ public class GmailTest {
 
 		driver.manage().window().maximize();
 
-		driver.findElement(By.cssSelector("input#identifierId")).sendKeys("tweetatkavi7@gmail.com");
+		driver.findElement(By.cssSelector("input#identifierId")).sendKeys("mail-Id@gmail.com");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 		driver.findElement(By.id("identifierNext")).click(); 
 
-		// driver.findElement(By.xpath("//div[@class=\"Xb9hP\"]")).sendKeys("Jan12018");
+		// driver.findElement(By.xpath("//div[@class=\"Xb9hP\"]")).sendKeys("your password");
 
 		WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
 		WebDriverWait wait = new WebDriverWait(driver, 20);
